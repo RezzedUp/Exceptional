7bfcd7849cb577bb0fddfc7aa42844b4dfea4bc2
RezzedUp <RezzedUp@users.noreply.github.com>
2021-06-30 15:15:06 -0500

    Add Attempt.rethrowing() methods. Add Attempt tests.

--- src/main/java/com/rezzedup/util/exceptional/Attempt.java
+++ src/main/java/com/rezzedup/util/exceptional/Attempt.java
@@ -122,4 +122,35 @@ public class Attempt
     {
         with(Catcher::print, action);
     }
+    
+    /**
+     * Attempts to return the supplied value by rethrowing
+     * any exceptions that occur.
+     *
+     * @param supplier  value supplier
+     *
+     * @param <T>       type of supplied value
+     *
+     * @return  the supplied value wrapped in an optional
+     *          or empty if an exception occurs
+     *
+     * @see Catcher#rethrow(Throwable)
+     */
+    public static <T> Optional<T> rethrowing(CheckedSupplier<@NullOr T, ? extends Exception> supplier)
+    {
+        return with(Catcher::rethrow, supplier);
+    }
+    
+    /**
+     * Attempts to run the action by rethrowing any
+     * exceptions that occur.
+     *
+     * @param action    action runnable
+     *
+     * @see Catcher#rethrow(Throwable)
+     */
+    public static void rethrowing(CheckedRunnable<? extends Exception> action)
+    {
+        with(Catcher::rethrow, action);
+    }
 }
---
+++ src/test/java/com/rezzedup/util/exceptional/AttemptTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/RezzedUp/Exceptional>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.rezzedup.util.exceptional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.function.IntSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttemptTests
+{
+    private static final IntSupplier ZERO = () -> 0;
+    
+    private int divideByZero() { return 1 / ZERO.getAsInt(); }
+    
+    private void throwsChecked() throws Exception { throw new Exception(); }
+    
+    private void expect(String message) { for (int i = 0; i < 3; i++) { System.err.println(message); } }
+    
+    @Test
+    public void testIgnoring()
+    {
+        Optional<Integer> divided = assertDoesNotThrow(() -> Attempt.ignoring(this::divideByZero));
+        assertTrue(divided.isEmpty());
+        
+        assertDoesNotThrow(() -> Attempt.ignoring(this::throwsChecked));
+    }
+    
+    @Test
+    public void testPrinting()
+    {
+        expect("THE STACKTRACES BELOW ARE EXPECTED.");
+        
+        Optional<Integer> divided = assertDoesNotThrow(() -> Attempt.printing(this::divideByZero));
+        assertTrue(divided.isEmpty());
+        
+        assertDoesNotThrow(() -> Attempt.printing(this::throwsChecked));
+        
+        expect("NO MORE STACKTRACES ARE EXPECTED.");
+    }
+    
+    @Test
+    public void testRethrow()
+    {
+        assertThrows(Rethrow.class, () -> Attempt.rethrowing(this::divideByZero));
+        assertThrows(Rethrow.class, () -> Attempt.rethrowing(this::throwsChecked));
+    }
+}
