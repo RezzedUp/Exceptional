f74159ad744a4f71d812337dacf11177bea7ef6d
RezzedUp <RezzedUp@users.noreply.github.com>
2022-01-31 20:25:02 -0600

    Copyright strikes again.

--- src/test/java/com/rezzedup/util/exceptional/ReadMeExampleTests.java
+++ src/test/java/com/rezzedup/util/exceptional/ReadMeExampleTests.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2021-2022, RezzedUp <https://github.com/RezzedUp/Exceptional>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.rezzedup.util.exceptional;
 
 import com.rezzedup.util.exceptional.checked.CheckedSupplier;
