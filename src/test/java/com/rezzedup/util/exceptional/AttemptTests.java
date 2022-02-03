a7dbad364a7edfc9f6a2ff1602d59d40aab798ed
RezzedUp <RezzedUp@users.noreply.github.com>
2022-02-03 16:37:37 -0600

    Improve AttemptTests.

--- src/test/java/com/rezzedup/util/exceptional/AttemptTests.java
+++ src/test/java/com/rezzedup/util/exceptional/AttemptTests.java
@@ -7,51 +7,115 @@
  */
 package com.rezzedup.util.exceptional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import pl.tlinkowski.annotation.basic.NullOr;
 
+import java.io.IOException;
 import java.util.Optional;
-import java.util.function.IntSupplier;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AttemptTests
 {
-    private static final IntSupplier ZERO = () -> 0;
-    
-    private int divideByZero() { return 1 / ZERO.getAsInt(); }
-    
-    private void throwsChecked() throws Exception { throw new Exception(); }
-    
-    private void expect(String message){ for (int i = 0; i < 3; i++) { System.err.println("\n" + message + "\n"); } }
-    
-    @Test
-    public void testIgnoring()
+    private static class Result<T>
     {
-        Optional<Integer> divided = assertDoesNotThrow(() -> Attempt.ignoring().get(this::divideByZero));
-        assertTrue(divided.isEmpty());
+        @NullOr T result;
         
-        assertDoesNotThrow(() -> Attempt.ignoring().run(this::throwsChecked));
+        void assertIfResultExists(Consumer<T> assertions)
+        {
+            if (result != null) { assertions.accept(result); }
+        }
+    }
+    
+    private static void expect(String message)
+    {
+        System.err.println("\n\n");
+        for (int i = 0; i < 3; i++) { System.err.println(message); }
+        System.err.println("\n\n");
+    }
+    
+    // The exceptional math logic *is* the point!
+    @SuppressWarnings({"divzero", "NumericOverflow"})
+    private void test(Attempt attempt, Function<Class<? extends Throwable>, Consumer<Executable>> assertions)
+    {
+        // Test a checked runnable that simply throws an Exception
+        {
+            assertions.apply(Exception.class).accept(
+                () -> attempt.run(() -> { throw new Exception(); })
+            );
+        }
+        
+        // Test a checked supplier that throws an IOException
+        {
+            Result<Optional<?>> maybe = new Result<>();
+            
+            assertions.apply(IOException.class).accept(() ->
+                maybe.result = attempt.get(() -> { throw new IOException(); })
+            );
+            
+            maybe.assertIfResultExists(optional -> assertTrue(optional.isEmpty()));
+        }
+        
+        // Test a checked int supplier that throws an ArithmeticException (divides by zero)
+        {
+            Result<OptionalInt> maybe = new Result<>();
+            
+            assertions.apply(ArithmeticException.class).accept(() ->
+                maybe.result = attempt.getAsInt(() -> 1 / 0)
+            );
+            
+            maybe.assertIfResultExists(optional -> assertTrue(optional.isEmpty()));
+        }
+        
+        // Test a checked long supplier that throws an ArithmeticException (divides by zero)
+        {
+            Result<OptionalLong> maybe = new Result<>();
+            
+            assertions.apply(ArithmeticException.class).accept(() ->
+                maybe.result = attempt.getAsLong(() -> 1L / 0L)
+            );
+            
+            maybe.assertIfResultExists(optional -> assertTrue(optional.isEmpty()));
+        }
     }
     
     @Test
-    public void testPrinting()
+    public void testIgnoringAttempt()
     {
-        expect("THE STACKTRACES BELOW ARE EXPECTED.");
-        
-        Optional<Integer> divided = assertDoesNotThrow(() -> Attempt.printing().get(this::divideByZero));
-        assertTrue(divided.isEmpty());
-        
-        expect("THESE STACKTRACES ARE EXPECTED.");
-        
-        assertDoesNotThrow(() -> Attempt.printing().run(this::throwsChecked));
-        
-        expect("NO MORE STACKTRACES ARE EXPECTED.");
+        test(Attempt.ignoring(), exception -> Assertions::assertDoesNotThrow);
     }
     
     @Test
-    public void testRethrow()
+    public void testPrintingAttempt()
     {
-        assertThrows(Rethrow.class, () -> Attempt.rethrowing().get(this::divideByZero));
-        assertThrows(Rethrow.class, () -> Attempt.rethrowing().run(this::throwsChecked));
+        expect("[!] THE STACKTRACES BELOW ARE EXPECTED (TESTING EXCEPTION PRINTING).");
+        test(Attempt.printing(), exception -> Assertions::assertDoesNotThrow);
+        expect("[!] THE STACKTRACES ABOVE WERE EXPECTED (TESTING EXCEPTION PRINTING).");
+    }
+    
+    @Test
+    public void testRethrowAttempt()
+    {
+        test(Attempt.rethrowing(), exception -> task -> assertThrows(Rethrow.class, task));
+    }
+    
+    @Test
+    public void testCustomizedSneakyAttempt()
+    {
+        test(Attempt.with(Sneaky::catcher), exception -> task -> assertThrows(exception, task));
+    }
+    
+    @SuppressWarnings({"ResultOfMethodCallIgnored", "ConstantConditions"})
+    @Test
+    public void customizedAttemptThrowsNullPointerException()
+    {
+        assertThrows(NullPointerException.class, () -> Attempt.with(null));
     }
 }
